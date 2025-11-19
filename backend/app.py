201f446c4f4b1dfa7d5408ae5e24764c1d29cedc
Park SooBin <soobin2930@gmail.com>
2025-11-19 11:18:04 +0900

    DB 서버화 테스트

--- backend/app.py
+++ backend/app.py
@@ -384,4 +384,5 @@ def login_user():
 # Flask 실행
 # -------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
